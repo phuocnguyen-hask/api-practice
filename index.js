e6cbe4bf6214b1452296a14b921c2e2f987475f7
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-03-05 00:31:30 +0700

    intial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
+
+app.listen(port, () => {
+    console.log(`App is running on port ${port}`);
+})
