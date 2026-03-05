fb2e1ec507452d9ba3953af5595db11ba087b513
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-03-05 12:22:24 +0700

    finish genre search feature

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import axios from "axios";
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const API_URL = "https://api.jikan.moe/v4/";
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", {pageCSS:""});
+    res.render("index.ejs", {pageCss:""});
+})
+
+app.get("/anime-search", async (req, res) => {
+    const genreId = req.query.genreId;
+    const result = (await axios.get(API_URL + `anime?genres=${genreId}`));
+    res.render("genre.ejs", {pageCss: "season.css", datas: result.data.data});
 })
 
 app.listen(port, () => {
