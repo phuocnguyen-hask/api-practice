1625594088981237e8804ec46330d262f076ab10
phuocnguyn203-wq <phuocnguyn203@gmail.com>
2026-03-05 10:24:36 +0700

    add pageCss in app.get

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = 3000;
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", {pageCSS:""});
 })
 
 app.listen(port, () => {
